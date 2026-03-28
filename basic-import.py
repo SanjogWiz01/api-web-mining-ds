e4b58fd47248fd3a0f68d0e55dd39edd6dfe4713
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-03-28 22:08:23 +0545

    basic api request

---
+++ basic-import.py
@@ -0,0 +1,13 @@
+import requests
+
+# Public API (no key needed)
+url = "https://jsonplaceholder.typicode.com/posts"
+
+response = requests.get(url)
+
+print("Status Code:", response.status_code)
+
+# Print first 2 results
+data = response.json()
+print(data[:2])
+# REQUEST API
